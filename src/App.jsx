05ca49f16522aea987ca7441cf14d016714e230f
judele124 <gwtlyby00@gmail.com>
2025-04-04 14:01:36 +0300

    add css

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,6 @@
-import { useState } from "react";
 import MilkyWayScene from "./milkyWay/MilkyWayScene";
-import { AppProvider } from "./protfilo/context/AppContext";
 function App() {
-  const [isMobile, setIsMobile] = useState(false);
-  return (
-    <AppProvider value={{ isMobile, setIsMobile }}>
-      <MilkyWayScene />
-    </AppProvider>
-  );
+  return <MilkyWayScene />;
 }
 
 export default App;
--- src/milkyWay/MilkyWayScene.jsx
+++ src/milkyWay/MilkyWayScene.jsx
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect } from "react";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import "./MilkyWayScene.css";
+import "./milkyWay.css";
 import gsap from "gsap";
 
 const MilkyWayScene = () => {
